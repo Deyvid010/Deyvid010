5a08f4122731fef8ed35cbc2fdebe36f9c3d9acf
Deyvid010 <43302444+Deyvid010@users.noreply.github.com>
2018-09-15 20:24:31 +0000

    Add files via upload

---
+++ LED.ino
@@ -0,0 +1,13 @@
+const int pinLed=13;
+void setup() {
+  // put your setup code here, to run once:
+pinMode (pinLed,OUTPUT);
+}
+
+void loop() {
+  int p=analogRead(A0);
+  int n=map(p,0,1024,20,5000);
+  tone (pinLed,n);
+  // put your main code here, to run repeatedly:
+
+}
